6c9abd775a59fdd178fc50df3b2161402d03ca00
Gauthier Ancelin <gauthier.ancelin@free.fr>
2022-07-21 00:14:36 +0200

    Simplification essai.adb

--- essai.adb
+++ essai.adb
@@ -9,11 +9,10 @@
 with text_io; use text_io;
 
 -- Essai compilation Linux/MacM1
--- commentaire ajoute
 procedure essai is
 begin
    put_line("Hello !");
-   for I in 0 .. 10 loop
+   for I in 1 .. 10 loop
       Put_Line("Linux MacM1");
    end loop;
 
